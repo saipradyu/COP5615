87e978177e67efaaeb4594676dca94950d2368c2
Pradyumna Reddy <pradyumnareddy.bits@gmail.com>
2020-09-30 13:04:27 -0400

    feat: build full and line topology

--- Proj2/project2.fsx
+++ Proj2/project2.fsx
@@ -13,9 +13,32 @@ let roundNodes n s =
     | "imp2d" -> Math.Pow(Math.Round(sqrt (float n)), 2.0) |> int
     | _ -> n
 
+let buildTopology n s =
+    let mutable map = Map.empty
+    match s with
+    | "full" ->
+        [ 1 .. n ]
+        |> List.map (fun x ->
+            let nlist = List.filter (fun y -> x <> y) [ 1 .. n ]
+            map <- map.Add(x, nlist))
+        |> ignore
+        map
+    | "line" ->
+        [ 1 .. n ]
+        |> List.map (fun x ->
+            let nlist =
+                List.filter (fun y -> (y = x + 1 || y = x - 1)) [ 1 .. n ]
+
+            map <- map.Add(x, nlist))
+        |> ignore
+        map
+    | "2d" -> map
+    | "imp2d" -> map
+    | _ -> map
+
 match fsi.CommandLineArgs.Length with
 | 4 -> ignore
-| _ -> failwith "Requires number of nodes, topology and algorithm as imput"
+| _ -> failwith "Requires number of nodes, topology and algorithm as input"
 
 let args = fsi.CommandLineArgs |> Array.tail
 let topology = args.[1]
